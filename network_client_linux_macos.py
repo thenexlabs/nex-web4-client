843f841c2f7e589f0cea817370895f073b61ace6
jonmacphers <jonmacphers@gmail.com>
2023-04-21 22:53:04 -0400

    added check for error ip ff:ff:ff:ff:ff:ff

--- network_client_linux_macos.py
+++ network_client_linux_macos.py
@@ -9,22 +9,22 @@ apiKey = sys.argv[1]
 def packet_callback(packet):
     # Check if packet has IP layer
     if IP in packet:
-        protocol_type = "IP"
+      protocol_type = "IP"
     # Check if packet has TCP layer
     elif TCP in packet:
-        protocol_type = "TCP"
+      protocol_type = "TCP"
     # Check if packet has UDP layer
     elif UDP in packet:
-        protocol_type = "UDP"
+      protocol_type = "UDP"
     # Check if packet has DNS layer
     elif DNS in packet:
-        protocol_type = "DNS"
+      protocol_type = "DNS"
     # Check if packet has Ether layer
     elif Ether in packet:
-        protocol_type = "Ether"
+      protocol_type = "Ether"
     # Otherwise, print unknown protocol type
     else:
-        protocol_type = "Unknown"
+      protocol_type = "Unknown"
     # print(packet.summary())
     # print(packet.fields)
     # print(packet[protocol_type].fields)
@@ -42,9 +42,9 @@ def packet_callback(packet):
     wrong_fragment = 0
     # Check if the protocol has the urgptr attribute
     if hasattr(packet[protocol_type], 'urgptr'):
-        urgent = packet[protocol_type].urgptr
+      urgent = packet[protocol_type].urgptr
     else:
-        urgent = 0
+      urgent = 0
     hot = 0
     num_failed_logins = 0
     logged_in = 0
@@ -80,102 +80,106 @@ def packet_callback(packet):
 
     # Extract required information from the packet
     if protocol_type == "TCP":
-        dst_bytes = packet[protocol_type].dlen if 'dlen' in packet[protocol_type].fields else None
-        land = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('WScale', 10), ('NOP', ()), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
-        wrong_fragment = packet[protocol_type].flags if 'flags' in packet[protocol_type].fields and packet[protocol_type].flags & 0x1 else None
-        hot = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('NOP', ()), ('WScale', 10), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
-        root_shell = packet[protocol_type].flags if 'flags' in packet[protocol_type].fields and packet[protocol_type].flags & 0x4000 else None
-        su_attempts = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('NOP', ()), ('WScale', 10), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
-        num_root = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('NOP', ()), ('WScale', 10), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
-        dst_bytes = packet[protocol_type].dsize
-        if packet[protocol_type].flags & 0x010:
-            land = 1
-        if packet[protocol_type].flags & 0x080:
-            num_failed_logins += 1
-        if packet[protocol_type].flags & 0x020:
-            logged_in = 1
-        if packet[protocol_type].flags & 0x008:
-            num_compromised += 1
-        if packet[protocol_type].flags & 0x002:
-            root_shell = 1
-        if packet[protocol_type].flags & 0x001:
-            su_attempted = 1
-        if packet[protocol_type].flags & 0x004:
-            num_root += 1
-        if packet[protocol_type].flags & 0x040:
-            num_file_creations += 1
-        src_bytes = len(packet[protocol_type].payload)
-        count = 1
-        srv_count = 1
-        serror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].serror)
-        srv_serror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].srv_serror)
-        rerror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].rerror)
-        srv_rerror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].srv_rerror)
-        same_srv_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].sprintf(packet[protocol_type].same_syn) / float(packet[protocol_type].sprintf(packet[protocol_type].count)))
-        diff_srv_rate = packet[protocol_type].sprintf("%.2f%%", (1 - float(packet[protocol_type].sprintf(packet[protocol_type].same_syn)) / float(packet[protocol_type].sprintf(packet[protocol_type].count))))
-        srv_diff_host_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].sprintf(packet[protocol_type].diff_syn) / float(packet[protocol_type].sprintf(packet[protocol_type].srv_count)))
-        dst_host_count = 1
-        dst_host_srv_count = 1
+      dst_bytes = packet[protocol_type].dlen if 'dlen' in packet[protocol_type].fields else None
+      land = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('WScale', 10), ('NOP', ()), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
+      wrong_fragment = packet[protocol_type].flags if 'flags' in packet[protocol_type].fields and packet[protocol_type].flags & 0x1 else None
+      hot = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('NOP', ()), ('WScale', 10), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
+      root_shell = packet[protocol_type].flags if 'flags' in packet[protocol_type].fields and packet[protocol_type].flags & 0x4000 else None
+      su_attempts = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('NOP', ()), ('WScale', 10), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
+      num_root = packet[protocol_type].options if 'options' in packet[protocol_type].fields and packet[protocol_type].options == [('MSS', 536), ('NOP', ()), ('WScale', 10), ('NOP', ()), ('Timestamp', (3547056421, 0))] else None
+      dst_bytes = packet[protocol_type].dsize
+      if packet[protocol_type].flags & 0x010:
+          land = 1
+      if packet[protocol_type].flags & 0x080:
+          num_failed_logins += 1
+      if packet[protocol_type].flags & 0x020:
+          logged_in = 1
+      if packet[protocol_type].flags & 0x008:
+          num_compromised += 1
+      if packet[protocol_type].flags & 0x002:
+          root_shell = 1
+      if packet[protocol_type].flags & 0x001:
+          su_attempted = 1
+      if packet[protocol_type].flags & 0x004:
+          num_root += 1
+      if packet[protocol_type].flags & 0x040:
+          num_file_creations += 1
+      src_bytes = len(packet[protocol_type].payload)
+      count = 1
+      srv_count = 1
+      serror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].serror)
+      srv_serror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].srv_serror)
+      rerror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].rerror)
+      srv_rerror_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].srv_rerror)
+      same_srv_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].sprintf(packet[protocol_type].same_syn) / float(packet[protocol_type].sprintf(packet[protocol_type].count)))
+      diff_srv_rate = packet[protocol_type].sprintf("%.2f%%", (1 - float(packet[protocol_type].sprintf(packet[protocol_type].same_syn)) / float(packet[protocol_type].sprintf(packet[protocol_type].count))))
+      srv_diff_host_rate = packet[protocol_type].sprintf("%.2f%%", packet[protocol_type].sprintf(packet[protocol_type].diff_syn) / float(packet[protocol_type].sprintf(packet[protocol_type].srv_count)))
+      dst_host_count = 1
+      dst_host_srv_count = 1
 
     # Send the collected information to the API endpoint
     api_url = "https://api.thenex.world/.netlify/functions/network-monitor-data"
     headers = {
-    'nex-api-key': apiKey,
-    'Content-type': 'application/json'
+      'nex-api-key': apiKey,
+      'Content-type': 'application/json'
     }
     now = datetime.datetime.now()
     data = {
-        "timestamp": int(now.timestamp()),
-        "src_ip": src_ip,
-        "dst_ip": dst_ip,
-        "src_port": src_port,
-        "dst_port": dst_port,
-        "duration": duration,
-        "protocol_type": protocol_type,
-        "service": service,
-        "flag": flag,
-        "src_bytes": src_bytes,
-        "dst_bytes": dst_bytes,
-        "land": land,
-        "wrong_fragment": wrong_fragment,
-        "urgent": urgent,
-        "hot": hot,
-        "num_failed_logins": num_failed_logins,
-        "logged_in": logged_in,
-        "num_compromised": num_compromised,
-        "root_shell": root_shell,
-        "su_attempted": su_attempted,
-        "num_root": num_root,
-        "num_file_creations": num_file_creations,
-        "num_shells": num_shells,
-        "num_access_files": num_access_files,
-        "num_outbound_cmds": num_outbound_cmds,
-        "is_host_login": is_host_login,
-        "is_guest_login": is_guest_login,
-        "count": count,
-        "srv_count": srv_count,
-        "serror_rate": serror_rate,
-        "srv_serror_rate": srv_serror_rate,
-        "rerror_rate": rerror_rate,
-        "srv_rerror_rate": srv_rerror_rate,
-        "same_srv_rate": same_srv_rate,
-        "diff_srv_rate": diff_srv_rate,
-        "srv_diff_host_rate": srv_diff_host_rate,
-        "dst_host_count": dst_host_count,
-        "dst_host_srv_count": dst_host_srv_count,
-        "dst_host_same_srv_rate": dst_host_same_srv_rate,
-        "dst_host_diff_srv_rate": dst_host_diff_srv_rate,
-        "dst_host_same_src_port_rate": dst_host_same_src_port_rate,
-        "dst_host_srv_diff_host_rate": dst_host_srv_diff_host_rate,
-        "dst_host_serror_rate": dst_host_serror_rate,
-        "dst_host_srv_serror_rate": dst_host_srv_serror_rate,
-        "dst_host_rerror_rate": dst_host_rerror_rate,
-        "dst_host_srv_rerror_rate": dst_host_srv_rerror_rate
+      "timestamp": int(now.timestamp()),
+      "src_ip": src_ip,
+      "dst_ip": dst_ip,
+      "src_port": src_port,
+      "dst_port": dst_port,
+      "duration": duration,
+      "protocol_type": protocol_type,
+      "service": service,
+      "flag": flag,
+      "src_bytes": src_bytes,
+      "dst_bytes": dst_bytes,
+      "land": land,
+      "wrong_fragment": wrong_fragment,
+      "urgent": urgent,
+      "hot": hot,
+      "num_failed_logins": num_failed_logins,
+      "logged_in": logged_in,
+      "num_compromised": num_compromised,
+      "root_shell": root_shell,
+      "su_attempted": su_attempted,
+      "num_root": num_root,
+      "num_file_creations": num_file_creations,
+      "num_shells": num_shells,
+      "num_access_files": num_access_files,
+      "num_outbound_cmds": num_outbound_cmds,
+      "is_host_login": is_host_login,
+      "is_guest_login": is_guest_login,
+      "count": count,
+      "srv_count": srv_count,
+      "serror_rate": serror_rate,
+      "srv_serror_rate": srv_serror_rate,
+      "rerror_rate": rerror_rate,
+      "srv_rerror_rate": srv_rerror_rate,
+      "same_srv_rate": same_srv_rate,
+      "diff_srv_rate": diff_srv_rate,
+      "srv_diff_host_rate": srv_diff_host_rate,
+      "dst_host_count": dst_host_count,
+      "dst_host_srv_count": dst_host_srv_count,
+      "dst_host_same_srv_rate": dst_host_same_srv_rate,
+      "dst_host_diff_srv_rate": dst_host_diff_srv_rate,
+      "dst_host_same_src_port_rate": dst_host_same_src_port_rate,
+      "dst_host_srv_diff_host_rate": dst_host_srv_diff_host_rate,
+      "dst_host_serror_rate": dst_host_serror_rate,
+      "dst_host_srv_serror_rate": dst_host_srv_serror_rate,
+      "dst_host_rerror_rate": dst_host_rerror_rate,
+      "dst_host_srv_rerror_rate": dst_host_srv_rerror_rate
     }
-    response = requests.post(api_url, headers=headers, json=data)
-    print(response.status_code)
-    print(response.json())
-    print('-----------------------------------------')
+    if(data.src_ip == 'ff:ff:ff:ff:ff:ff'){
+    }else if(data.dst_ip == 'ff:ff:ff:ff:ff:ff'){
+    }else{
+      response = requests.post(api_url, headers=headers, json=data)
+      print(response.status_code)
+      print(response.json())
+      print('-----------------------------------------')
+    }
     # print(data)
 
 # Sniff the network traffic
