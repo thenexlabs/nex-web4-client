b6b40f606f9adbd095a3cf42017b6fce692d008e
jonmacphers <jonmacphers@gmail.com>
2023-04-24 15:56:55 -0400

    added gui

--- network_client_linux_macos.py
+++ network_client_linux_macos.py
@@ -1,33 +1,28 @@
 import requests
 from scapy.all import *
-import sys
 import datetime
 from tkinter import *
 from threading import Thread
 
-# Read the api key from the command line
-apiKey = sys.argv[1]
 
-# GUI
-root = Tk()
-root.geometry("300x100")
-root.configure(bg='black')
-root.title("The NEX Web4 Client")
-
+# Global variable to store api key
+apiKey = None
 # Global variable to control whether the sniff function should continue running
 running = True
+# Global variable to store api json response message
+apiResponseMessage = None
+# Global variable to store tkinter textarea for printing
+text = None
 
-def Close():
-    global running
-    # Set the 'running' flag to False to stop the sniff function
-    running = False
-    # Destroy the Tkinter window
-    root.destroy()
+# print function for tkinter GUI
+def printGUI(string):
+  global text
+  if(text):
+    text.insert('1.0', '{}\n'.format(string))
+  else:
+    print(string)
 
-exit_button = Button(root, text="Stop", command=Close)
-exit_button.pack(pady=20)
-exit_button.configure(bg='black', fg='lime')
-
+# sniff callback
 def packet_callback(packet):
     # Check if packet has IP layer
     if IP in packet:
@@ -142,6 +137,7 @@ def packet_callback(packet):
 
       # Send the collected information to the API endpoint
       api_url = "https://api.thenex.world/.netlify/functions/network-monitor-data"
+      global apiKey
       headers = {
         'nex-api-key': apiKey,
         'Content-type': 'application/json'
@@ -199,25 +195,89 @@ def packet_callback(packet):
       # }else if(data.dst_ip == 'ff:ff:ff:ff:ff:ff'){
       # }else{
       response = requests.post(api_url, headers=headers, json=data)
-      print(response.status_code)
-      print(response.json())
-      print('-----------------------------------------')
+      responseJson = response.json()
+      
+      global apiResponseMessage
+      if(not apiResponseMessage and responseJson['message']):
+        apiResponseMessage = responseJson['message']
+      
+      printGUI('-----------------------------------------')
+      printGUI(responseJson)
+      # printGUI(response.status_code)
+      
       # }
       # print(data)
     except ExceptionType:
-      print(ExceptionType)
+      printGUI(ExceptionType)
+
+
+# GUI
+root = Tk()
+root.geometry("400x300")
+root.configure(bg='black')
+root.title("The NEX Web4 Client")
+
+
+def start_sniff():
+  # grab apikey from input
+  global apiKey
+  apiKey = apikey_entry.get()
+  printGUI(f'API key: {apiKey}')
+
+  if not apiKey:
+    printGUI("Please enter an API key.")
+    return
+
+  # Disable the start button while the sniff function is running
+  start_button.config(state=DISABLED)
+
+  # Start the sniff function in a separate thread
+  t = Thread(target=sniff_thread)
+  t.start()
+
 
 def stop_sniff(packet):
-    # Return True to stop sniffing when the 'running' flag is False
-    return not running
+  # Return True to stop sniffing when the 'running' flag is False
+  if(apiResponseMessage == 'Unauthorized'):
+    printGUI("Please enter a valid API key.")
+    return True
+
+  # Return True to stop sniffing when the 'running' flag is False
+  return not running
+
 
 def sniff_thread():
-    # Sniff packets until the 'stop_sniff' function returns True
-    sniff(prn=packet_callback, store=0, stop_filter=stop_sniff)
+  # Sniff packets until the 'stop_sniff' function returns True
+  sniff(prn=packet_callback, store=0, stop_filter=stop_sniff)
+  # Re-enable the start button when the sniff function stops
+  start_button.config(state=NORMAL)
 
-# Start the sniff function in a separate thread
-t = Thread(target=sniff_thread)
-t.start()
+
+def Close():
+    global running
+    # Set the 'running' flag to False to stop the sniff function
+    running = False
+    # Destroy the Tkinter window
+    root.destroy()
+
+
+start_button = Button(root, text="Start", command=start_sniff)
+start_button.pack(pady=10)
+start_button.configure(bg='black', fg='lime')
+
+apikey_label = Label(root, text="Enter your API key:")
+apikey_label.pack()
+apikey_label.configure(bg='black', fg='lime')
+apikey_entry = Entry(root)
+apikey_entry.pack()
+
+exit_button = Button(root, text="Stop", command=Close)
+exit_button.pack(pady=20)
+exit_button.configure(bg='black', fg='lime')
+
+text = Text(root)
+text.pack()
+text.configure(bg='black', fg='lime')
 
 # Run the GUI
 root.mainloop()
